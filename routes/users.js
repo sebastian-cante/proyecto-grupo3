e7b7ef4f0cc98635b59e094ff3f991af6ea9e29d
NahuelVaz <navazon44@gmail.com>
2020-06-24 21:31:02 -0300

    rutas de register y login creadas

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
 });
 
+router.get('/logIn', function(req, res, next) {
+  res.render('logIn');
+});
 module.exports = router;
