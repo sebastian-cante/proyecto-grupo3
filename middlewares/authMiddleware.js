adce4614768b08e1764003b06df276a6a6349f91
sebastian-cante <sebastian.cantelmi@hotmail.com>
2020-07-19 23:14:43 -0300

    se realizaron cambios a nivel visual de la página, además se agregó vista para cuando el usuario desea ingresar al carrito pero no está logueado

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ function authMiddleware(req, res, next){
     if (req.session.userLoggued != undefined){
         next()
     } else{
-        res.send('Solo pueden ingresar a esta página los usuarios logueados')
+        res.render('solicitud_login')
     }
 }
 
